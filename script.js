05a598c2caa04ecf9e5100a8ae9d2d9ef6d8caad
M, Javad Delavari <mjdelaavaarii@gmail.com>
2023-05-10 22:10:18 +0430

    update

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
-const http = require("http")
 
 const pic = fs.readFileSync('./pic.jpg')
 
-http.createServer((req,res) => {
-    res.writeHead(200,"Content-Type:text/plain");
-    res.write(`${pic}`)
-}).listen(8080)
 
+const dataGenerator = function* (data = pic) {
+    for(let i = 0 ; i < data.length ; i++){
+        yield data.slice(i,i+1)
+    }
+}
 
-const buffer = pic.slice(0,10)
-console.log(buffer);
+const generatedData = [...dataGenerator()]
+console.log(generatedData);
