b2e3befe492073a616f0bc526a8be42c27f1d6fb
M, Javad Delavari <mjdelaavaarii@gmail.com>
2023-05-11 11:53:23 +0430

    update

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const pic = fs.readFileSync('./pic.jpg')
 
 
 const dataGenerator = function* (data = pic) {
-    for(let i = 0 ; i < data.length ; i++){
-        yield data.slice(i,i+1)
+    for(let i = 0 ; i <= data.length ; i+=1000){
+        yield data.slice(i,i+1000)
     }
 }
-
-const generatedData = [...dataGenerator()]
+const data = [...dataGenerator()]
+const generatedData = Buffer.concat(data)
+const compare = Buffer.compare(generatedData,pic)
+console.log(data);
 console.log(generatedData);
+console.log(compare);
